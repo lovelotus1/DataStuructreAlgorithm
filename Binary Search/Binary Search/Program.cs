7d49a991c40cfe15b90a437a32b09dfe210f8fde
“Love <opera.prem@gmail.com>
2022-08-25 20:53:15 +0430

    Added:Binary Search the Word from Word List

---
+++ Binary Search/Binary Search/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BinarySearch;
+
+
+class Program
+{
+
+    // Returns index of x if it is present in arr[],
+    // else return -1
+    static int binarySearch(String[] arr, String x)
+    {
+        int l = 0, r = arr.Length - 1;
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+
+            int res = x.CompareTo(arr[m]);
+
+            // Check if x is present at mid
+            if (res == 0)
+                return m;
+
+            // If x greater, ignore left half
+            if (res > 0)
+                l = m + 1;
+
+            // If x is smaller, ignore right half
+            else
+                r = m - 1;
+        }
+
+        return -1;
+    }
+
+    // Driver method to test above
+    public static void Main(String[] args)
+    {
+        String[] arr = { "cat", "bat", "car", "mobile" };
+        String x = "bat";
+        int result = binarySearch(arr, x);
+
+        if (result == -1)
+            Console.WriteLine("Element not present");
+        else
+            Console.WriteLine("Element found at "
+                            + "index " + result);
+    }
+}
