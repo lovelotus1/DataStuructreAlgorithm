1bd70fe01091b889cfafc1090b37dd95d0320f55
“Love <opera.prem@gmail.com>
2022-09-02 09:17:02 +0430

    InsertionSortProgram

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sortarray = new int[5] { 23, 67, 9, 22, 23 };
+            int n = 5, value, flag, i, j;
+            Console.WriteLine("Before Sorting");
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(sortarray[i] + " ");
+
+            }
+            Console.WriteLine();
+
+            for (i = 0; i < n; i++)
+            {
+                value = sortarray[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (value < sortarray[j])
+                    {
+                        sortarray[j + 1] = sortarray[j];
+                        j--;
+                        sortarray[j + 1] = value;
+                    }
+                    else
+                        flag = 1;
+
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
