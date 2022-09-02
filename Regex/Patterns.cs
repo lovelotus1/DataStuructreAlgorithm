33c2bc80752ed9d7c12ae069ea7b4a782d495f9f
“Love <opera.prem@gmail.com>
2022-09-02 09:35:47 +0430

    RegexPatternProgram

---
+++ Regex/Patterns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex
+{
+    public class Patterns
+    {
+        public static string Regex_Patterns = "^[0-9]{1}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, Regex_Patterns);
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the string");
+            string pincode = Console.ReadLine();
+            Patterns patterns = new Patterns();
+            if (patterns.validatePinCode(pincode))
+                Console.WriteLine("Pattern Matched");
+            else
+                Console.WriteLine("Pattern Not Matched");
+        }
+    }
+}
