f67efeb1a670c4f65af354f8d13bccd5a74bb9a1
“Love <opera.prem@gmail.com>
2022-09-02 09:28:28 +0430

    PrimeNumberProgram

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Problems");
+            Console.WriteLine("Press 6 for Primenumber Problem");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            PrimeNumberRange pnum = new PrimeNumberRange();
+            pnum.Prime();
+        }
+    }
+}
