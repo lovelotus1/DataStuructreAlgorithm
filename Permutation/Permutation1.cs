6460ad53d1cf5303bbfdc569a383ae00740e6988
“Love <opera.prem@gmail.com>
2022-09-02 09:25:05 +0430

    PermutationProgram

---
+++ Permutation/Permutation1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutation
+{
+
+    public class Permutation1
+    {
+        // method for permute with string str int left and int right
+        // permutation function
+        // str string to
+        // calculate permutation for
+        // l starting index r end index
+
+        public static void Permute(String str, int startingIndex, int endIndex)
+        {
+            //if startindex ==  endindex print str
+            if (startingIndex == endIndex)
+                Console.WriteLine(str);
+            else
+            {
+
+                for (int var_i = startingIndex; var_i <= endIndex; var_i++)
+                {
+                    str = Swap(str, startingIndex, var_i);
+                    Permute(str, startingIndex + 1, endIndex);
+                    str = Swap(str, startingIndex, var_i);
+                }
+            }
+        }
+        // Swap Characters at position
+        // inputForWord string value
+        // left position 1
+        // var_i position 2
+
+        public static string Swap(string inputForWord, int left, int var_i)
+        {
+            char temp;
+            char[] charArray = inputForWord.ToCharArray();
+            temp = charArray[left];
+            charArray[left] = charArray[var_i];
+            charArray[var_i] = temp;
+            string Output = new string(charArray);
+            return Output;
+        }
+    }
+}
---
+++ Permutation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Permutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n5.Permutation");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                //Permutation
+                Permutation1 permutation = new Permutation1();
+                String str = "Vaibhav";
+                int Outputs = str.Length;
+                Permutation1.Permute(str, 0, Outputs - 1);
+                Console.ReadKey();
+            }
+        }
+    }
+}
